53a1cfe3ccae7c56605157cd48f5a436b12e7509
Takashi Idobe <idobetakashi@gmail.com>
2018-06-13 20:00:52 -0500

    switched to stack implementation, but still buggy

--- numberNames.js
+++ numberNames.js
@@ -4,19 +4,13 @@
 //optional: support for inputs other than positive numbers as well
 
 const numberNames = (num = 0) => {
-  //returns the amount of digits in the number
-  const numLength = num.toString().split('').length;
-  //returns the digits of the number split 
-  const splitNum = num.toString().split('').map((x) => parseInt(x));
 
-  //general formula to determine a number
-  let fullWord = prefix + suffix + endNumber;
-  let prefix;
-  let suffix;
-  let endNumber;
+  //returns the digits of the number split 
+  let splitNum = num.toString().split('');
+  //returns the amount of digits in the number
+  let numLength = splitNum.length;
 
   //to piece together the numbers zero to 10
-  const zero = 'zero';
   const one = 'one';
   const two = 'two';
   const three = 'three';
@@ -28,113 +22,161 @@ const numberNames = (num = 0) => {
   const nine = 'nine';
   const ten = 'ten';
 
-  //to piece together the numbers 13-19
-  const teen = 'teen';
-  const twoPrefix = 'twen';
-  const threePrefix = 'thir';
-  const fourPrefix = four;
-  const fivePrefix = 'fif';
-  const sixPrefix = six;
-  const sevenPrefix = seven;
-  const eightPrefix = eight;
-  const ninePrefix = nine;
+  //general roots
+  const twoRoot = 'twen';
+  const threeRoot = 'thir';
+  const fourRoot = four;
+  const fiveRoot = 'fif';
+  const sixRoot = six;
+  const sevenRoot = seven;
+  const eightRoot = eight;
+  const nineRoot = nine;
+  const tenRoot = ten;
 
-  //to piece together the numbers from twenty to a hundred
+  //suffixes to use
+  let tens = 'ty';
+  let hundred = 'hundred';
+  let thousands = 'thousand';
 
-  //numbers from 0 to 9
+  if (num > -1 && num < 20) {
+    if (num === 0) { return 'zero'; }
+    else if (num === 1) { return 'one'; }
+    else if (num === 2) { return 'two'; }
+    else if (num === 3) { return 'three'; }
+    else if (num === 4) { return 'four'; }
+    else if (num === 5) { return 'five'; }
+    else if (num === 6) { return 'six'; }
+    else if (num === 7) { return 'seven'; }
+    else if (num === 8) { return 'eight'; }
+    else if (num === 9) { return 'nine'; }
+    else if (num === 10) { return 'ten'; }
+    else if (num === 11) { return 'eleven'; }
+    else if (num === 12) { return 'twelve'; }
+    else if (num === 13) { return 'thirteen'; }
+    else if (num === 14) { return 'fourteen'; }
+    else if (num === 15) { return 'fifteen'; }
+    else if (num === 16) { return 'sixteen'; }
+    else if (num === 17) { return 'seventeen'; }
+    else if (num === 18) { return 'eighteen'; }
+    else if (num === 19) { return 'nineteen'; }
+  }
+
+  let fullWord = [];
+
+
+  if (numLength === 6) {
+    //100000; ['1', '0', '0', '0', '0', '0']; 6
+    if (splitNum[0] === '1') { fullWord.push(`${one}-${hundred}`); }
+    else if (splitNum[0] === '2') { fullWord.push(`${two}-${hundred}`); }
+    else if (splitNum[0] === '3') { fullWord.push(`${three}-${hundred}`); }
+    else if (splitNum[0] === '4') { fullWord.push(`${four}-${hundred}`); }
+    else if (splitNum[0] === '5') { fullWord.push(`${five}-${hundred}`); }
+    else if (splitNum[0] === '6') { fullWord.push(`${six}-${hundred}`); }
+    else if (splitNum[0] === '7') { fullWord.push(`${seven}-${hundred}`); }
+    else if (splitNum[0] === '8') { fullWord.push(`${eight}-${hundred}`); }
+    else if (splitNum[0] === '9') { fullWord.push(`${nine}-${hundred}`); }
+    splitNum.shift();
+  }
+
+  numLength = splitNum.length;
+
+  if (numLength === 5) {
+    //10000; [1,0,0,0,0]; 5
+    //push nothing if 0
+    if (splitNum[0] === '0') { fullWord.push(``) }
+    else if (splitNum[0] === '1') { fullWord.push(`eleven`); }
+    else if (splitNum[0] === '2') { fullWord.push(`${twoRoot}${tens}`); }
+    else if (splitNum[0] === '3') { fullWord.push(`${threeRoot}${tens}`); }
+    else if (splitNum[0] === '4') { fullWord.push(`${fourRoot}${tens}`); }
+    else if (splitNum[0] === '5') { fullWord.push(`${fiveRoot}${tens}`); }
+    else if (splitNum[0] === '6') { fullWord.push(`${sixRoot}${tens}`); }
+    else if (splitNum[0] === '7') { fullWord.push(`${sevenRoot}${tens}`); }
+    else if (splitNum[0] === '8') { fullWord.push(`${eightRoot}${tens}`); }
+    else if (splitNum[0] === '9') { fullWord.push(`${nineRoot}${tens}`); }
+    splitNum.shift();
+  }
+
+  numLength = splitNum.length;
+
+  if (numLength === 4) {
+    if (splitNum[0] === '0') { fullWord.push(`-${thousands}`); }
+    else if (splitNum[0] === '1') { fullWord.push(`${one}-${thousands}`); }
+    else if (splitNum[0] === '2') { fullWord.push(`${two}-${thousands}`); }
+    else if (splitNum[0] === '3') { fullWord.push(`${three}-${thousands}`); }
+    else if (splitNum[0] === '4') { fullWord.push(`${four}-${thousands}`); }
+    else if (splitNum[0] === '5') { fullWord.push(`${five}-${thosuands}`); }
+    else if (splitNum[0] === '6') { fullWord.push(`${six}-${thousands}`); }
+    else if (splitNum[0] === '7') { fullWord.push(`${seven}-${thousands}`); }
+    else if (splitNum[0] === '8') { fullWord.push(`${eight}-${thousands}`); } 
+    else if (splitNum[0] === '9') { fullWord.push(`${nine}-${thousands}`); }
+    splitNum.shift();
+  }
+
+  numLength = splitNum.length; 
+
+  if (numLength === 3) {
+    // if (splitNum[0] === '0') { fullWord.push(`${hundred}`); }
+    if (splitNum[0] === '1') { fullWord.push(`${one}-${hundred}`); }
+    else if (splitNum[0] === '2') { fullWord.push(`${two}-${hundred}`); }
+    else if (splitNum[0] === '3') { fullWord.push(`${three}-${hundred}`); }
+    else if (splitNum[0] === '4') { fullWord.push(`${four}-${hundred}`); }
+    else if (splitNum[0] === '5') { fullWord.push(`${five}-${hundred}`); }
+    else if (splitNum[0] === '6') { fullWord.push(`${six}-${hundred}`); }
+    else if (splitNum[0] === '7') { fullWord.push(`${seven}-${hundred}`); }
+    else if (splitNum[0] === '8') { fullWord.push(`${eight}-${hundred}`); }
+    else if (splitNum[0] === '9') { fullWord.push(`${nine}-${hundred}`); }
+    splitNum.shift();
+  }
+
+  numLength = splitNum.length;
+
+  if (numLength === 2) {
+    if (splitNum[0] === '1') { 
+        if (splitNum[1] === '0') { fullWord.push(`ten`); }
+        else if (splitNum[1] === '1') { fullWord.push(`eleven`); }
+        else if (splitNum[1] === '2') { fullWord.push(`twelve`); }
+        else if (splitNum[1] === '3') { fullWord.push(`thirteen`); }
+        else if (splitNum[1] === '4') { fullWord.push(`fourteen`); }
+        else if (splitNum[1] === '5') { fullWord.push(`fifteen`); }
+        else if (splitNum[1] === '6') { fullWord.push(`sixteen`); }
+        else if (splitNum[1] === '7') { fullWord.push(`seventeen`); }
+        else if (splitNum[1] === '8') { fullWord.push(`eighteen`); }
+        else if (splitNum[1] === '9') { fullWord.push(`nineteen`); }
+        splitNum = [];
+    }
+    else if (splitNum[0] === '2') { fullWord.push(`${twoRoot}${tens}`); }
+    else if (splitNum[0] === '3') { fullWord.push(`${threeRoot}${tens}`); }
+    else if (splitNum[0] === '4') { fullWord.push(`${fourRoot}${tens}`); }
+    else if (splitNum[0] === '5') { fullWord.push(`${fiveRoot}${tens}`); }
+    else if (splitNum[0] === '6') { fullWord.push(`${sixRoot}${tens}`); }
+    else if (splitNum[0] === '7') { fullWord.push(`${sevenRoot}${tens}`); }
+    else if (splitNum[0] === '8') { fullWord.push(`${eightRoot}${tens}`); }
+    else if (splitNum[0] === '9') { fullWord.push(`${nineRoot}${tens}`); }
+    splitNum.shift();
+  }
+
+  numLength = splitNum.length;
+
   if (numLength === 1) {
-    if (num === 0) { return `${zero}`; }
-    else if (num === 1) { return `${one}`; }
-    else if (num === 2) { return `${two}`; }
-    else if (num === 3) { return `${three}`; }
-    else if (num === 4) { return `${four}`; }
-    else if (num === 5) { return `${five}`; }
-    else if (num === 6) { return `${six}`; }
-    else if (num === 7) { return `${seven}`; }
-    else if (num === 8) { return `${eight}`; }
-    else if (num === 9) { return `${nine}`; }
+    //if the digit is 0, push nothing
+    if (splitNum[0] === '0') { fullWord.push(``); }
+    else if (splitNum[0] === '1') { fullWord.push(`-${one}`); }
+    else if (splitNum[0] === '2') { fullWord.push(`-${two}`); }
+    else if (splitNum[0] === '3') { fullWord.push(`-${three}`); }
+    else if (splitNum[0] === '4') { fullWord.push(`-${four}`); }
+    else if (splitNum[0] === '5') { fullWord.push(`-${five}`); }
+    else if (splitNum[0] === '6') { fullWord.push(`-${six}`); }
+    else if (splitNum[0] === '7') { fullWord.push(`-${seven}`); }
+    else if (splitNum[0] === '8') { fullWord.push(`-${eight}`); }
+    else if (splitNum[0] === '9') { fullWord.push(`-${nine}`); }
+    splitNum.shift();
   }
 
-  //for all numbers from 10 to 99
-  else if (numLength === 2) {
-    if (num === 10) { return `${ten}`; }
-    else if (num === 11) { return `eleven`; }
-    else if (num === 12) { return `twelve`; }
-
-    //piece together teen and the prefix
-    //TODO more abstraction
-    else if (num >= 13 && num <= 19) {
-      if (num === 13) { return `${threePrefix}${teen}`; }
-      else if (num === 14) { return `${fourPrefix}${teen}`; }
-      else if (num === 15) { return `${fivePrefix}${teen}`; }
-      else if (num === 16) { return `${sixPrefix}${teen}`; }
-      else if (num === 17) { return `${sevenPrefix}${teen}`; }
-      else if (num === 18) { return `${eightPrefix}${teen}`; }
-      else if (num === 19) { return `${ninePrefix}${teen}`; }
-    }
-
-    //numbers 20 to 100
-    else if (num >= 20 && num <= 99) {
-      //make a factory that puts the words together
-      //21 => twoPrefix + tenSuffix + endNum;
-      suffix = 'ty';
-
-      if (splitNum[0] === 2) { prefix = twoPrefix; }
-      else if (splitNum[0] === 3) { prefix = threePrefix; }
-      else if (splitNum[0] === 4) { prefix = fourPrefix; }
-      else if (splitNum[0] === 5) { prefix = fivePrefix; }
-      else if (splitNum[0] === 6) { prefix = sixPrefix; }
-      else if (splitNum[0] === 7) { prefix = sevenPrefix; }
-      else if (splitNum[0] === 8) { prefix = eightPrefix; }
-      else if (splitNum[0] === 9) { prefix = ninePrefix; }
-      
-      if (splitNum[1] === 0) { endNumber = ''; }
-      else if (splitNum[1] === 1) { endNumber = two; }
-      else if (splitNum[1] === 2) { endNumber = three; }
-      else if (splitNum[1] === 3) { endNumber = four; }
-      else if (splitNum[1] === 4) { endNumber = five; }
-      else if (splitNum[1] === 5) { endNumber = six; }
-      else if (splitNum[1] === 6) { endNumber = seven; }
-      else if (splitNum[1] === 7) { endNumber = eight; }
-      else if (splitNum[1] === 8) { endNumber = nine; }
-
-      return `${prefix}${suffix}${endNumber}`;
-    }
-
-    else if (num >= 100 && num <= 999) {
-      //ninehundred-ninety-nine
-      let hundred = 'hundred';
-      let tensSuffix = 'ty';
-      if (splitNum[0] === 1) { prefix = one; }
-      else if (splitNum[0] === 2) { prefix = two; }
-      else if (splitNum[0] === 3) { prefix = three; }
-      else if (splitNum[0] === 4) { prefix = four; }
-      else if (splitNum[0] === 5) { prefix = five; }
-      else if (splitNum[0] === 6) { prefix = six; }
-      else if (splitNum[0] === 7) { prefix = seven; }
-      else if (splitNum[0] === 8) { prefix = eight; }
-      else if (splitNum[0] === 9) { prefix = nine; }
-
-    
-
-
-
-      return `${prefix}-${hundred}${suffix}${tenSuffix}${endNumber}`;
-
-    }
-
-
-  //for numbers 1000 to 10000
-
-  //for numbers 10000 to 100000
-
-  //for numbers 100000 to 1000000
-
-
-  //74.79 == 'seventy-four.seventy-nine'
-  //1 == 'one'
-  //10 == 'ten'
-  //11 == 'eleven'
+  numLength = splitNum.length;
+  if (numLength === 0) {
+    return fullWord.join(' ');
   }
+}
+
   
 
